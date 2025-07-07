99354be56948ed55f4ef11e09f6b955661f7b342
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2025-07-07 14:45:30 +0700

    setup auth

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import { createNewUser, signin } from "./handlers/user";
+import { protect } from "./modules/auth";
 import router from "./router";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use("/api", router);
+app.use("/api", protect, router);
+
+app.post("/user", createNewUser);
+app.post("/signin", signin);
+
+export default app;
