703070d34a0d5e68d692559872dbb4cb74ccc396
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2025-07-08 11:14:54 +0700

    deploy

---
+++ src/handlers/__tests__/user.test.ts
@@ -0,0 +1,5 @@
+describe("user handler", () => {
+  it("tessss", () => {
+    expect(1).toBe(1);
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
+import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import { createNewUser, signin } from "./handlers/user";
 import { protect } from "./modules/auth";
 import router from "./router";
 
 const app = express();
-const PORT = 3000;
 
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+  setTimeout(() => {
+    next(new Error("hello"));
+  }, 1);
+});
+
 app.use("/api", protect, router);
 
 app.post("/user", createNewUser);
